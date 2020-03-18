c6b954757070a5634a6695fc6ddb51bbeaf3b262
Magnus Gudmundsson <MGUDMUN3@volvocars.com>
2020-03-18 16:30:16 +0100

    ok alles klar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ListService } from './list.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fragteste';
+export class AppComponent implements OnInit {
+  list: [];
+  selectedPage = '';
+  @ViewChild('favDialog', { static: true }) favDialog: ElementRef;
+
+  constructor(private listService: ListService) { }
+
+  ngOnInit(): void {
+    this.listService.get().subscribe(itm =>
+      this.list = itm);
+  }
+
+  load(uri: string): void {
+    this.listService.getFile(uri).subscribe(itm => {
+      this.selectedPage = itm;
+      this.favDialog.nativeElement.showModal();
+    });
+  }
+
 }
